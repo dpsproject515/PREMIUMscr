4f1807a7e177d57524123794c2c8259521afc1df
dpsproject515 <dpsproject.oficial@gmail.com>
2026-06-12 21:52:59 +0700

    Create dpspro.sh

---
+++ dpspro.sh
@@ -0,0 +1,453 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+echo
+echo
+echo
+clear
+echo "
+╔════════════════════════════════════╗
+
+║  ██████╗ ██████╗ ███████╗          ║
+
+║  ██╔══██╗██╔══██╗██╔════╝          ║
+
+║  ██║  ██║██████╔╝███████╗          ║
+
+║  ██║  ██║██╔═══╝ ╚════██║          ║
+
+║  ██████╔╝██║     ███████║          ║
+
+║  ╚═════╝ ══╝          ║
+
+║      PRIVATE TERMINAL SYSTEM         ║
+
+╚════════════════════════════════════╝
+
+"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     IDN PRIVATE PHONEFARM UNITY"$i"         |"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku" DPS CYBER UNITY  [DPSproject515]"$i"          |"
+echo $i" |"$pu" FACEBOOK :"$ku" Dps project"$i"                   |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 087757839224"$i"             |"
+echo $i" |"$pu" Contack Gmail :"$ku" dpsproject.oficial@gmail.com"$i"      |"
+echo $i" |"$pu" Kata Mutiara :"$ku" Tidak cocok untuk pengguna gratisan"$i"      |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" TIKTOK VIEW LIVESTREAM   "$i"|"$i"    |"$cy" P"$i" |"
+echo $i"|"$me" 2"$i" |"$cy" TIKTOK COMMENT CUSTOM    "$i"|"$i"    |"$cy" R"$i" |"
+echo $i"|"$me" 3"$i" |"$cy" TIKTOK SHARE REAL        "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me" 4"$i" |"$cy" TIKTOK TAP AUTO          "$i"|"$i"    |"$cy" M"$i" |"
+echo $i"|"$me" 5"$i" |"$cy" TIKTOK LIKE LIVESTREAM   "$i"|"$i"    |"$cy" I"$i" |"
+echo $i"|"$me" 6"$i" |"$cy" TIKTOK CLONING APP       "$i"|"$i"    |"$cy" U"$i" |"
+echo $i"────────────────────────────────"$i"   |"$cy" M"$i" |"
+echo $i"|"$me" 7"$i" |"$cy" SHOPEE VIEW LIVESTREAM   "$i"|"$i"    ─────"
+echo $i"|"$me" 8"$i" |"$cy" SHOPEE COMMENT CUSTOM    "$i"|"$i"    |"$cy" B"$i" |"
+echo $i"|"$me" 9"$i" |"$cy" SHOPEE LIKE LIVESTREAM   "$i"|"$i"    |"$cy" O"$i" |"
+echo $i"|"$me"10"$i" |"$cy" FREEREELS REMOVE ADS     "$i"|"$i"    |"$cy" O"$i" |"
+echo $i"|"$me"11"$i" |"$cy" FREERERLS CLONING APPS   "$i"|"$i"    |"$cy" S"$i" |"
+echo $i"|"$me"12"$i" |"$cy" YOUTUBE VIEW FOR MONET   "$i"|"$i"    |"$cy" T"$i" |"
+echo $i"|"$me"13"$i" |"$cy" YOUTUBE BOOST SUBSCRIBE  "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" R"$i" |"
+echo $i"|"$me"14"$i" |"$cy" SERVER TIKTOK 1          "$i"|"$i"    ─────"
+echo $i"|"$me"15"$i" |"$cy" SERVER TOKTOK 2          "$i"|"$i"
+echo $i"|"$me"16"$i" |"$cy" SERVER SHOPEE 1          "$i"|"$i"    |"$cy" S"$i" |"
+echo $i"|"$me"17"$i" |"$cy" SERVER SHOPEE 2          "$i"|"$i"    |"$cy" C"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" R"$i" |"
+echo $i"|"$me"18"$i" |"$cy" INSTALL AUTOMATION       "$i"|"$i"    |"$cy" I"$i" |"
+echo $i"|"$me"19"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    |"$cy" P"$i" |"
+echo $i"────────────────────────────────    ─────"
+echo
+echo $me"┌==="$bi"["$i"DPSproject515"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/fbbrute
+cd fbbrute
+php fb.php
+fi
+
+
+if [ $pil = 2 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+
+cd Fb-Cracker-v.3
+
+python2 crack.py
+
+fi
+
+
+if [ $pil = 3 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/IqbalzNoobs/fb-brute
+
+cd fb-brute
+
+python2 brute.py
+
+fi
+
+
+if [ $pil = 4 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/FR13ND8/mbf
+
+cd mbf
+
+python2 MBF.py
+
+fi
+
+
+if [ $pil = 5 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/N1ght420/FaceBrute
+
+cd FaceBrute
+
+python fb.py
+
+fi
+
+
+if [ $pil = 6 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/thelinuxchoice/facebash.git
+
+cd facebash
+
+bash facebash.sh
+
+fi
+
+
+if [ $pil = 7 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/FR13ND8/EmailVuln
+
+cd EmailVuln
+
+python2 vuln.py
+
+fi
+
+
+if [ $pil = 8 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/FR13ND8/ProfileGuardFb
+
+cd ProfileGuardFb
+
+php guard.php
+
+fi
+
+
+if [ $pil = 9 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/IlayTamvan/Report
+
+cd Report
+
+unzip Report.zip
+
+python2 Report.py
+
+fi
+
+
+if [ $pil = 10 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/AMVengeance/FB-React
+
+cd FB-React
+
+./start
+
+fi
+
+
+if [ $pil = 11 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/FR13ND8/InfoFB
+
+cd InfoFB
+
+pip2 install -r requirements.txt
+
+python2 info.py
+
+fi
+
+
+if [ $pil = 12 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+pip2 install mechanize
+
+git clone https://github.com/Senitopeng/Botkomena.git
+
+cd Botkomena
+
+python2 botkomena.py
+
+fi
+
+
+if [ $pil = 13 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/FR13ND8/autolike
+
+cd autolike
+
+php autolike.php
+
+fi
+
+
+if [ $pil = 14 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/thelinuxchoice/shellphish.git
+
+cd shellphish
+
+bash shellphish.sh
+
+fi
+
+
+if [ $pil = 15 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/thelinuxchoice/blackeye.git
+
+cd blackeye
+
+bash blackeye.sh
+
+fi
+
+
+if [ $pil = 16 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+git clone https://github.com/UndeadSec/SocialFish.git
+
+cd SocialFish
+
+chmod +x *
+
+pip2 install -r requirements.txt
+
+python2 SocialFish.py
+
+fi
+
+
+if [ $pil = 17 ]
+
+then
+
+clear
+
+figlet -f slant "W A I T"|lolcat
+
+sleep 1
+
+https://github.com/evait-security/weeman.git
+
+chmod +x *
+
+python2 weeman.py
+
+fi
+
+
+if [ $pil = 18 ]
+
+then
+
+clear
+
+apt update && apt upgrade
+
+apt install python2
+
+pip2 install urllib3 chardet certifi idna requests
+
+pkg install git
+
+pip2 install mechanize
+
+pkg install curl
+
+pkg install ruby
+
+pkg install gem
+
+gem install lolcat
+
+pkg install git
+
+pkg install php
+
+pkg install ruby cowsay toilet figlet
+
+pkg install neofetch
+
+pkg install nano
+
+figlet -f slant " S U K S E S "|lolcat
+
+fi
+
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Terima Kasih atas kunjungannya"
+sleep 2
+echo $i"Bila Ada kebutuhan lainnya anda bisa hubungi kami"
+sleep 2
+echo $ku"WhatsApp :"$i" 087757839224"
+echo $ku"Facebook :"$i" Dps project"
+sleep 2
+echo $pur">> NOTE !! Setiap usaha dan perjuangan butuh pengorbanan yang tidak mudah untuk dilakukan <<"
+exit
+fi
